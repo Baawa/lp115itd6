29cbad61bb389b720c90d809f02232f372f8a8c4
kristofferek <inzeer@gmail.com>
2015-09-07 17:55:09 +0200

    Lab2
    Förbättrad Rse //Klarar av testRse

--- Kristoffer/Rse.java
+++ Kristoffer/Rse.java
@@ -1,32 +1,26 @@
 //Grupp 16, Kristoffer Ek, Albin Bååw
 public class Rse {
-	static int sum=0;
 	static boolean isEqual=false;
-	static int row[];
-	static int temp=0;
+	static int result=0;
 	
 	public static boolean allRowSumsEqual(int[][] m){
-		for(int i=0;i<m.length;i++){
-			row=null;
-			row= m[i];
-			if(temp==rowSum(row)){
-				isEqual = true;
-			}else{
-				isEqual = false;
-			}
-			temp=rowSum(row);
+		if(m==null || m.length<=1) return true;
+		
+		int temp = rowSum(m[0]);
+		
+		for(int[] i : m){
+			if(temp==rowSum(i)) isEqual=true;
+			else return false;
 		}
 		return isEqual;
 	}
 	public static int rowSum(int[] v){
-		sum=0;
-		if(row==null){
-			sum=0;
-		}else{
-		for(int numb : row){
-		sum+=numb;
+		result=0;
+		if(v!=null){
+		for(int num : v) {
+			result += num;
 		}
-		}
-		return sum;
+		}else return 0;
+		return result;
 	}
 }
